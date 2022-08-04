60cfd60ac1abc5dfe745ea768c190aaa527ee87c
Henus321 <tyrant321@yandex.ru>
2022-08-04 23:43:18 +0300

    cart temporary styled

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,24 +5,38 @@ import { FaUser } from 'react-icons/fa';
 import { v4 as uuidv4 } from 'uuid';
 
 import './header.styles.scss';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { CollectionContext } from '../../contexts/collection.context';
 
 const Header = () => {
   const { cart, deleteFromCart, changeQuantity } =
     useContext(CollectionContext);
   const [cartActive, setCartActive] = useState(false);
+  const [heartSpanActive, setHeartSpanActive] = useState(false);
 
-  const toggleProfileItem = () => {
+  const toggleCart = () => {
     setCartActive(!cartActive);
   };
 
-  console.log(cart);
+  useEffect(() => {
+    cart.length > 0 ? setHeartSpanActive(true) : setHeartSpanActive(false);
+  }, [cart.length]);
 
   return (
     <header className="header">
       {cartActive && (
         <div className="header__cart">
+          <div className="header__cart-filter"></div>
+          <div className="header__cart-heading">
+            <h2 className="header__cart-title">Корзина</h2>
+            {/* <span>{cart.length} Товар</span> */}
+            <span
+              className="header__cart-close-btn"
+              onClick={() => toggleCart()}
+            >
+              x
+            </span>
+          </div>
           {cart.length > 0 ? (
             cart.map((cartItem) => (
               <div className="header__cart-item" key={uuidv4()}>
@@ -31,25 +45,48 @@ const Header = () => {
                   alt={cartItem.name}
                   className="header__cart-photo"
                 />
-                <span>{cartItem.name}</span>
-                <span>{cartItem.price}&#8381;</span>
-                <span>Цвет: {cartItem.color}</span>
-                <span>Размер: {cartItem.size}</span>
-                <div>
-                  <span>Количество: </span>
-                  <button onClick={() => changeQuantity(cartItem, -1)}>
-                    -
-                  </button>
-                  <span>{cartItem.quantity}</span>
-                  <button onClick={() => changeQuantity(cartItem, +1)}>
-                    +
+                <div className="header__cart-description">
+                  <span className="header__cart-item-name">
+                    {cartItem.name}
+                  </span>
+                  <span className="header__cart-text">
+                    {cartItem.price}&#8381;
+                  </span>
+                  <span className="header__cart-text">
+                    Цвет: {cartItem.color}
+                  </span>
+                  <span className="header__cart-text">
+                    Размер: {cartItem.size}
+                  </span>
+                  <div>
+                    <span className="header__cart-text">Количество: </span>
+                    <button
+                      className="header__cart-btn"
+                      onClick={() => changeQuantity(cartItem, -1)}
+                    >
+                      -
+                    </button>
+                    <span className="header__cart-text">
+                      {cartItem.quantity}
+                    </span>
+                    <button
+                      className="header__cart-btn"
+                      onClick={() => changeQuantity(cartItem, +1)}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <button
+                    className="header__cart-delete-btn"
+                    onClick={() => deleteFromCart(cartItem)}
+                  >
+                    x
                   </button>
                 </div>
-                <button onClick={() => deleteFromCart(cartItem)}>x</button>
               </div>
             ))
           ) : (
-            <div>No Items Found</div>
+            <h2 className="header__cart-warning">В корзине нет товаров</h2>
           )}
         </div>
       )}
@@ -68,12 +105,25 @@ const Header = () => {
       </nav>
       <h2 className="header__title">Boney James</h2>
       <div className="header__profile">
-        <FaHeart className="header__icon" />
-        <FaShoppingBag
-          className="header__icon"
-          onClick={() => toggleProfileItem()}
-        />
-        <FaUser className="header__icon" />
+        <div className="header__icon">
+          <FaHeart />
+          <span className="header__heart-span"></span>
+        </div>
+        <div className="header__icon">
+          <FaShoppingBag
+            className="header__icon"
+            onClick={() => toggleCart()}
+          />
+          <span
+            className={
+              heartSpanActive ? 'header__bag-span--active' : 'header__bag-span'
+            }
+          ></span>
+        </div>
+        <div className="header__icon">
+          <FaUser className="header__icon" />
+          <span className="header__user-span"></span>
+        </div>
       </div>
     </header>
   );
