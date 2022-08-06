85180387db41650d92101601caba4e1bcedcf2d3
Henus321 <tyrant321@yandex.ru>
2022-08-06 23:11:13 +0300

    Cart moved from the header to its own component

---
+++ src/components/cart/cart.component.jsx
@@ -0,0 +1,101 @@
+import { useEffect } from 'react';
+import {
+  deleteFromCart,
+  changeQuantity,
+  toggleCart,
+  setCartTotal,
+  toggleCartStatus,
+} from '../../store/cart/cart.actions';
+import { selectCart } from '../../store/cart/cart.selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+
+import './cart.styles.scss';
+
+const Cart = () => {
+  const { cart, isCartActive, cartTotal } = useSelector(selectCart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const defaultCartTotal = 0;
+    cart.length > 0
+      ? dispatch(toggleCartStatus(true))
+      : dispatch(toggleCartStatus(false));
+    if (cart.length > 0) {
+      const total = cart.reduce((acc, item) => {
+        const curValue = item.price * item.quantity;
+        return acc + curValue;
+      }, 0);
+      dispatch(setCartTotal(total));
+    }
+    if (cart.length === 0) dispatch(setCartTotal(defaultCartTotal));
+  }, [dispatch, cart]);
+
+  return (
+    <div className={isCartActive ? 'cart--active' : 'cart'}>
+      <div className="cart__heading">
+        <h2 className="cart__title">Корзина</h2>
+        {/* Добавить количество предметов */}
+        {/* <span>{cart.length} Товар</span> */}
+        <span
+          className="cart__close-btn"
+          onClick={() => dispatch(toggleCart(!isCartActive))}
+        >
+          x
+        </span>
+      </div>
+      {cart.length > 0 ? (
+        cart.map((cartItem) => (
+          <div className="cart__item" key={uuidv4()}>
+            <img
+              src={cartItem.mainPhotoUrl}
+              alt={cartItem.name}
+              className="cart__photo"
+            />
+            <div className="cart__description">
+              <span className="cart__item-name">{cartItem.name}</span>
+              <span className="cart__text">{cartItem.price}&#8381;</span>
+              <span className="cart__text">Цвет: {cartItem.color}</span>
+              <span className="cart__text">Размер: {cartItem.size}</span>
+              <div>
+                <span className="cart__text">Количество: </span>
+                <button
+                  className="cart__btn"
+                  onClick={() => dispatch(changeQuantity(cartItem, -1))}
+                >
+                  -
+                </button>
+                <span className="cart__text">{cartItem.quantity}</span>
+                <button
+                  className="cart__btn"
+                  onClick={() => dispatch(changeQuantity(cartItem, +1))}
+                >
+                  +
+                </button>
+              </div>
+              <button
+                className="cart__delete-btn"
+                onClick={() => dispatch(deleteFromCart(cartItem))}
+              >
+                x
+              </button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h2 className="cart__warning">В корзине нет товаров</h2>
+      )}
+      {cart.length > 0 && (
+        <div className="cart__order">
+          <div className="cart__order-price">
+            <span>Итого</span>
+            <span>{cartTotal}&#8381;</span>
+          </div>
+          <button className="cart__order-accept">Оформить заказ</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,39 +1,19 @@
-import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaHeart } from 'react-icons/fa';
 import { FaShoppingBag } from 'react-icons/fa';
 import { FaUser } from 'react-icons/fa';
-import { v4 as uuidv4 } from 'uuid';
+
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  deleteFromCart,
-  changeQuantity,
-  toggleCart,
-  setCartTotal,
-  toggleCartStatus,
-} from '../../store/cart/cart.actions';
+import { toggleCart } from '../../store/cart/cart.actions';
 import { selectCart } from '../../store/cart/cart.selectors';
+
+import Cart from '../cart/cart.component';
 
 import './header.styles.scss';
 
 const Header = () => {
-  const { cart, isCartActive, cartTotal, cartStatus } = useSelector(selectCart);
+  const { isCartActive, cartStatus } = useSelector(selectCart);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const defaultCartTotal = 0;
-    cart.length > 0
-      ? dispatch(toggleCartStatus(true))
-      : dispatch(toggleCartStatus(false));
-    if (cart.length > 0) {
-      const total = cart.reduce((acc, item) => {
-        const curValue = item.price * item.quantity;
-        return acc + curValue;
-      }, 0);
-      dispatch(setCartTotal(total));
-    }
-    if (cart.length === 0) dispatch(setCartTotal(defaultCartTotal));
-  }, [dispatch, cart]);
 
   return (
     <header className="header">
@@ -45,78 +25,7 @@ const Header = () => {
         }
         onClick={() => dispatch(toggleCart(!isCartActive))}
       ></div>
-      <div className={isCartActive ? 'header__cart--active' : 'header__cart'}>
-        <div className="header__cart-heading">
-          <h2 className="header__cart-title">Корзина</h2>
-          {/* Добавить количество предметов */}
-          {/* <span>{cart.length} Товар</span> */}
-          <span
-            className="header__cart-close-btn"
-            onClick={() => dispatch(toggleCart(!isCartActive))}
-          >
-            x
-          </span>
-        </div>
-        {cart.length > 0 ? (
-          cart.map((cartItem) => (
-            <div className="header__cart-item" key={uuidv4()}>
-              <img
-                src={cartItem.mainPhotoUrl}
-                alt={cartItem.name}
-                className="header__cart-photo"
-              />
-              <div className="header__cart-description">
-                <span className="header__cart-item-name">{cartItem.name}</span>
-                <span className="header__cart-text">
-                  {cartItem.price}&#8381;
-                </span>
-                <span className="header__cart-text">
-                  Цвет: {cartItem.color}
-                </span>
-                <span className="header__cart-text">
-                  Размер: {cartItem.size}
-                </span>
-                <div>
-                  <span className="header__cart-text">Количество: </span>
-                  <button
-                    className="header__cart-btn"
-                    onClick={() => dispatch(changeQuantity(cartItem, -1))}
-                  >
-                    -
-                  </button>
-                  <span className="header__cart-text">{cartItem.quantity}</span>
-                  <button
-                    className="header__cart-btn"
-                    onClick={() => dispatch(changeQuantity(cartItem, +1))}
-                  >
-                    +
-                  </button>
-                </div>
-                <button
-                  className="header__cart-delete-btn"
-                  onClick={() => dispatch(deleteFromCart(cartItem))}
-                >
-                  x
-                </button>
-              </div>
-            </div>
-          ))
-        ) : (
-          <h2 className="header__cart-warning">В корзине нет товаров</h2>
-        )}
-        {cart.length > 0 && (
-          <div className="header__cart-order">
-            <div className="header__cart-order-price">
-              <span>Итого</span>
-              <span>{cartTotal}&#8381;</span>
-            </div>
-            <button className="header__cart-order-accept">
-              Оформить заказ
-            </button>
-          </div>
-        )}
-      </div>
-
+      <Cart />
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__nav-item">
