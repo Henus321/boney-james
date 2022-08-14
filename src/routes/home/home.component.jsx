9716891576d2eb768d91d2095163b45797bd3d23
Henus321 <tyrant321@yandex.ru>
2022-08-14 18:08:00 +0300

    home page images squashed

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
-import home_one from '../../assets/home_one.jpg';
-import home_two from '../../assets/home_two.jpg';
-// ПЕРЕЖАТЬ ФОТО ВЕСЯТ МНОГО
+import home_image_one from '../../assets/home_image_one.jpg';
+import home_image_two from '../../assets/home_image_two.jpg';
 
 import './home.styles.scss';
 
@@ -9,14 +8,14 @@ const Home = () => {
   return (
     <div className="home">
       <Link className="home__item" to="/collection/autumn">
-        <img className="home__image" src={home_two} alt="Осень" />
+        <img className="home__image" src={home_image_two} alt="Осень" />
         <div className="home__title-box">
           <span className="home__title">Коллекция</span>
           <span className="home__sub-title">Осень 2021</span>
         </div>
       </Link>
       <Link className="home__item" to="/collection/spring">
-        <img className="home__image" src={home_one} alt="Весна" />
+        <img className="home__image" src={home_image_one} alt="Весна" />
         <div className="home__title-box">
           <span className="home__title">Коллекция</span>
           <span className="home__sub-title">Весна 2022</span>
