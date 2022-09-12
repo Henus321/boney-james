4f16ed8ed1941d710764cac60fa076d8ad0654f9
Henus321 <tyrant321@yandex.ru>
2022-09-12 16:31:07 +0300

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Loader from './components/loader/loader.component';
+
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
-import Home from './routes/home/home.component';
-import About from './routes/about/about.component';
-import Collection from './routes/collection/collection.component';
-import Item from './routes/item/item.component';
-import Bookmarks from './routes/bookmarks/bookmarks.component';
-import Checkout from './routes/checkout/checkout.component';
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const About = lazy(() => import('./routes/about/about.component'));
+const Collection = lazy(() =>
+  import('./routes/collection/collection.component')
+);
+const Item = lazy(() => import('./routes/item/item.component'));
+const Bookmarks = lazy(() => import('./routes/bookmarks/bookmarks.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
 
 function App() {
   return (
@@ -17,17 +23,19 @@ function App() {
       <ToastContainer position="bottom-right" closeOnClick autoClose={1500} />
       <div className="container">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/collection/:year/:season" element={<Collection />} />
-          <Route
-            path="/collection/:year/:season/item/:coatId"
-            element={<Item />}
-          />
-          <Route path="/bookmarks" element={<Bookmarks />} />
-          <Route path="checkout" element={<Checkout />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/collection/:year/:season" element={<Collection />} />
+            <Route
+              path="/collection/:year/:season/item/:coatId"
+              element={<Item />}
+            />
+            <Route path="/bookmarks" element={<Bookmarks />} />
+            <Route path="checkout" element={<Checkout />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </>
